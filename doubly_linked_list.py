e8fc550bc3baa6905ea4e32bdf89a3dfe4160351
ahmedgamalelazab <agemy844@gmail.com>
2026-05-12 03:02:32 +0300

    feat: added unit testing and tested dll with fully pass results

---
+++ doubly_linked_list.py
@@ -0,0 +1,97 @@
+# Doubly linked list implementation
+class Node:
+    def __init__(self, data, next, prev):
+        self.data = None if data == None else data
+        self.next = None if next == None else next
+        self.prev = None if prev == None else prev
+        
+        
+class DoublyLinkedList:
+    def __init__(self, initial_data = None):
+        self.head = self.tail = Node(initial_data, None, None) if initial_data else None
+        
+    def get_head(self):
+        return self.head
+
+    def get_tail(self):
+        return self.tail
+    
+    def insert_on_head(self, data):
+        if self.head == self.tail and self.head == None:
+            self.head = self.tail = Node(data, None, None)
+        else:
+            head_ptr = self.head
+            self.head = Node(data, self.head, None)
+            head_ptr.prev = self.head
+            head_ptr = None
+    
+    def insert_on_tail(self, data):
+        if self.tail == self.head and self.tail == None:
+            self.head = self.tail = Node(data, None, None)
+        else:
+            tail_ptr = self.tail
+            self.tail = Node(data, None, tail_ptr)
+            tail_ptr.next = self.tail
+            tail_ptr = None
+        
+    def remove_from_head(self):
+        if (self.head == self.tail) and (self.head or self.tail):
+            self.head = self.tail = None
+        else:
+            head_ptr = self.head
+            self.head = self.head.next
+            self.head.prev = None
+            head_ptr.next = None
+            head_ptr = None
+    
+    def remove_from_tail(self):
+        if (self.head == self.tail) and (self.head or self.tail):
+            self.head = self.tail = None
+        else:
+            tail_ptr = self.tail
+            self.tail = self.tail.prev
+            self.tail.next = None
+            tail_ptr.prev = None
+            tail_ptr = None
+    
+    def remove(self, data):
+        if self.head == self.tail and self.head == None:
+            return False
+        else:
+            head_ptr = self.head
+            found_node = None
+            while head_ptr != None:
+                if head_ptr.data == data:
+                    found_node = head_ptr
+                    break
+                else:
+                    head_ptr = head_ptr.next
+            
+            if found_node:
+                found_ptr = found_node
+                if found_ptr.prev == found_ptr.next and found_ptr.next == None:
+                    self.head = self.tail = None
+                    found_ptr = None
+                    found_node = None
+                    return True
+                else:
+                    if found_ptr:
+                        if self.head == found_ptr:
+                            self.head = self.head.next
+                            self.head.prev = None
+                        if self.tail == found_ptr:
+                            self.tail = self.tail.prev
+                            self.tail.next = None
+                        if found_ptr.prev:
+                            found_ptr.prev.next = found_ptr.next if found_ptr.next else None
+                        if found_ptr.next:
+                            found_ptr.next.prev = found_ptr.prev if found_ptr.prev else None
+                        found_node.next = None
+                        found_node.prev = None
+                        found_node = None
+                        return True     
+            else:
+                return False                
+    
+    def insert(self, idx, data):
+        raise Exception("Method Not implemented yet")        
---
+++ test_doubly_linked_list.py
@@ -0,0 +1,135 @@
+
+from doubly_linked_list import DoublyLinkedList
+
+def test_initialization_of_dll():
+    dll = DoublyLinkedList(10)
+    assert dll != None
+    assert dll.get_head().data == 10
+    
+def test_stack_simulation_lifo():
+    dll = DoublyLinkedList()
+    items_to_push = [10,20,30]
+    for item in items_to_push:
+        dll.insert_on_tail(item)
+    # for lifo we need to chck the tail
+    assert dll.get_tail().data == items_to_push[-1]
+            
+
+def test_queue_simulation_fifo():
+    dll = DoublyLinkedList()
+    items_to_queue = [10,20,30]
+    for item in items_to_queue:
+        dll.insert_on_tail(item)
+    assert dll != None
+    # for fifo we need to check on head
+    assert dll.get_head().data == items_to_queue[0]
+    
+def test_remove_on_head():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_head(item)
+    dll.remove_from_head()
+    assert dll.get_head().data == 30
+
+def test_remove_all_items_from_head_and_check_emptiness():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_head(item)
+    
+    for i in range(len(items)):
+        dll.remove_from_head()
+    
+    assert dll.get_head() == dll.get_tail()
+    assert dll.get_head() == None
+    assert dll.get_tail() == None
+
+def test_remove_all_items_from_tail_and_check_emptiness():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_tail(item)
+    
+    for i in range(len(items)):
+        dll.remove_from_tail()
+    
+    assert dll.get_head() == dll.get_tail()
+    assert dll.get_head() == None
+    assert dll.get_tail() == None
+    
+    
+def test_remove_last_element_using_remove_fn():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_head(item)
+    
+    removed = dll.remove(40)
+    assert removed == True
+    
+    assert dll.get_head().data == 30
+    
+def test_remove_first_element_using_remove_fn():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_tail(item)
+    
+    removed = dll.remove(10)
+    assert removed == True
+    
+    assert dll.get_head().data == 20
+
+
+def test_remove_from_middle_element_using_remove_fn():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_tail(item)
+    
+    removed = dll.remove(20)
+    assert removed == True
+    
+    assert dll.get_head().data == 10
+    assert dll.get_head().next.data == 30
+    assert dll.get_head().next.next.data == 40
+    assert dll.get_tail().data == 40
+
+
+def test_remove_from_and_simulate_clean():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_tail(item)
+    
+    remove_results = []
+    
+    for item in items:
+        res = dll.remove(item)        
+        remove_results.append(res)
+        
+    assert dll.get_head() == dll.get_tail() == None
+    for rmr in remove_results:
+        assert rmr == True
+        
+        
+def test_remove_from_and_simulate_clean_then_try_to_remove():
+    dll = DoublyLinkedList()
+    items = [10,20,30,40]
+    for item in items:
+        dll.insert_on_tail(item)
+    
+    remove_results = []
+    
+    for item in items:
+        res = dll.remove(item)        
+        remove_results.append(res)
+        
+    assert dll.get_head() == dll.get_tail() == None
+    for rmr in remove_results:
+        assert rmr == True
+    
+    res = dll.remove(100)
+    
+    assert res == False
